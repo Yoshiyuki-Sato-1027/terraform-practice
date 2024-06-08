a55e5ebb5fb11209bcdd776b5a1a5cf22b0c3413
satou-yosiyuki <stuysyk1027@icloud.com>
2024-06-08 15:36:25 +0900

    EC2 Instance Start

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  profile = "terraform"
+  region = "us-east-1"
+}
+
+# ec2の起動
+resource "aws_instance" "hello-world" {
+  # イメージの指定
+  ami           = "ami-0d191299f2822b1fa"
+  instance_type = "t2.micro"
+}
