27a8fde8fafce2deffe9bdf378c432e0d864eda0
satou-yosiyuki <stuysyk1027@icloud.com>
2024-06-09 15:44:26 +0900

    InternetGateway

--- network.tf
+++ network.tf
@@ -107,10 +107,28 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_rt_1a" {
   route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private_subnet_1a.id
-
 }
 resource "aws_route_table_association" "private_rt_1c" {
   route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private_subnet_1c.id
+}
+
+#---------------------
+# Internet Gateway
+#---------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+# パブリックルートテーブルとインターネットゲートウェイを接続する
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 
 }
