4aa3bfef216482f195bb7caa4738a514699dca3f
satou-yosiyuki <stuysyk1027@icloud.com>
2024-07-06 00:52:40 +0900

    モジュールの変更

---
+++ book/main.tf
@@ -0,0 +1,13 @@
+
+# ============
+# モジュールを使用する terraform initかterraorm getをやって使える
+# ============
+module "web_server" {
+
+  source        = "./http_server"
+  instance_type = "t3.micro"
+}
+
+output "public_dns" {
+  value = module.web_server.public_dns
+}
