b9566041d0310a71a00b371e63f834439e229ce8
Yoshiyuki-Sato-1027 <stuysyk1027@icloud.com>
2024-11-17 01:43:43 +0900

    feat: :sparkles:プライベートバケットの作成

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform1"
+}
+
+resource "aws_s3_bucket_versioning" "private" {
+  bucket = aws_s3_bucket.private.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+  bucket = aws_s3_bucket.private.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
