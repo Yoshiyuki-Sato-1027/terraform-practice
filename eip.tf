ed68c001296c8ed732b3db63d964558e2d801a84
Yoshiyuki-Sato-1027 <stuysyk1027@icloud.com>
2024-11-17 21:00:00 +0900

    feat: :sparkles:マルチAZ

--- eip.tf
+++ eip.tf
@@ -1,7 +1,25 @@
-resource "aws_eip" "nat_gateway" {
+resource "aws_eip" "nat_gateway_0" {
   depends_on = [aws_internet_gateway.example]
   tags = {
     Name = "nat_gateway"
   }
 }
 
+resource "aws_eip" "nat_gateway_1" {
+  depends_on = [aws_internet_gateway.example]
+  tags = {
+    Name = "nat_gateway"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on    = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.example]
+}
--- subnet.tf
+++ subnet.tf
@@ -1,17 +1,31 @@
 # パブリックサブネット
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true # パブリックIPを自動で割り当てる
   availability_zone       = "ap-northeast-1a"
 }
 
-# プライベートサブネット
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true # パブリックIPを自動で割り当てる
+  availability_zone       = "ap-northeast-1c"
+}
+
+# プライベートサブネット
+resource "aws_subnet" "private_0" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.65.0/24"
   map_public_ip_on_launch = true # パブリックIPを自動で割り当てる
   availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
 }
 
 # 10.0.0.0/8の場合、ホスト部分が32-8=24で24bit、最初のアドレス10.0.0.0、最後のアドレス10.255.255.255
