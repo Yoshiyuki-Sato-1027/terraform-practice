cae6c98017afc35d1b118136a156af9492ea8430
Yoshiyuki-Sato-1027 <stuysyk1027@icloud.com>
2024-11-17 21:06:20 +0900

    feat: :sparkles:セキュリティグループの作成

---
+++ security_group.tf
@@ -0,0 +1,25 @@
+# セキュリティグループの定義
+resource "aws_security_group" "example" {
+  name   = "exapmle-sg"
+  vpc_id = aws_vpc.example.id
+}
+
+# インバウンドルール
+resource "aws_security_group_rule" "ingress_example" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+# アウトバウンドルール
+resource "aws_security_group_rule" "egress_example" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
