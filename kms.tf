f7f6dc3d8eb156802f572564f63ef9f220400df1
Yoshiyuki-Sato-1027 <stuysyk1027@icloud.com>
2024-12-07 15:05:34 +0900

    feat: :sparkles:kmsのエイリアスの作成

--- kms.tf
+++ kms.tf
@@ -1,6 +1,12 @@
 resource "aws_kms_key" "example" {
   description             = "Example Customer Master Key"
-  enable_key_rotation     = true
+  enable_key_rotation     = true # 年に一度自動ローテーション
   is_enabled              = true
   deletion_window_in_days = 30
+}
+
+resource "aws_kms_alias" "example" {
+
+  name          = "alias/example"
+  target_key_id = aws_kms_key.example.key_id
 }
